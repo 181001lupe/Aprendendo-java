c31709b8be8a1a70f7992e0fbfffe744367f3233
Saulo Duarte <sauloduarte45@gmail.com>
2025-10-23 11:51:00 -0300

    Tabuada.

---
+++ src/AtividadesFundamentosJava/Exercicio05.java
@@ -0,0 +1,12 @@
+package AtividadesFundamentosJava;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        int num = 3;
+        for (int i = 0; i <= 10; i++) {
+            int resultado = num * i;
+            System.out.println(num+" x "+i+ " = "+resultado);
+        }
+
+    }
+}
