3dc7424b581a030559b31afa5b73a263d5989e1b
Saulo Duarte <sauloduarte45@gmail.com>
2025-10-24 11:10:46 -0300

    Contagem personalizada.

---
+++ src/AtividadesFundamentosJava/Exercicio10.java
@@ -0,0 +1,12 @@
+package AtividadesFundamentosJava;
+
+public class Exercicio10 {
+    public static void main(String[] args) {
+        int numeroInicial = 2;
+        int numeroFinal = 15;
+        int incremento = 3;
+        for (int i = numeroInicial; i <= numeroFinal; i+= incremento) {
+            System.out.println(i);
+        }
+    }
+}
